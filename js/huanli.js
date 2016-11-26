b4cd08e0548f962b9098d5a480e8308daba294f0
BelieverW <hlwang0309@hotmail.com>
2016-11-26 15:08:05 -0500

    finished compare section

--- js/huanli.js
+++ js/huanli.js
@@ -55,11 +55,42 @@ function chooseHeroLeft(thisHero) {
     d3.select(".hero_show_left")
         .attr("src", gif_path);
     var img_name_show = img_name.split('-').join(' ').toUpperCase();
-    d3.select(".hero_left")
-        .select(".hero_name")
+    d3.select("#hero_name_left")
         .select("a")
         .text(img_name_show);
     }
+    d3.select("#line-left")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["lane"] * 10);
+    d3.select("#dps-left")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["dps"] * 10);
+    d3.select("#push-left")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["push"] * 10);
+    d3.select("#nuke-left")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["nuke"] * 10);
+    d3.select("#durable-left")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["durable"] * 10);
+    d3.select("#initial-left")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["initial"] * 10);
+    d3.select("#disable-left")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["disable"] * 10);
+    d3.select("#healing-left")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["healing"] * 10);
+    $("#line-left").barfiller({});
+    $("#dps-left").barfiller({});
+    $("#push-left").barfiller({});
+    $("#nuke-left").barfiller({});
+    $("#durable-left").barfiller({});
+    $("#initial-left").barfiller({});
+    $("#disable-left").barfiller({});
+    $("#healing-left").barfiller({});
 }
 
 function chooseHeroRight(thisHero) {
@@ -117,11 +148,42 @@ function chooseHeroRight(thisHero) {
     d3.select(".hero_show_right")
         .attr("src", gif_path);
     var img_name_show = img_name.split('-').join(' ').toUpperCase();
-    d3.select(".hero_right")
-        .select(".hero_name")
+    d3.select("#hero_name_right")
         .select("a")
         .text(img_name_show);
     }
+    d3.select("#line-right")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["lane"] * 10);
+    d3.select("#dps-right")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["dps"] * 10);
+    d3.select("#push-right")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["push"] * 10);
+    d3.select("#nuke-right")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["nuke"] * 10);
+    d3.select("#durable-right")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["durable"] * 10);
+    d3.select("#initial-right")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["initial"] * 10);
+    d3.select("#disable-right")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["disable"] * 10);
+    d3.select("#healing-right")
+        .select(".fill")
+        .attr("data-percentage", hero_attributes[img_name]["healing"] * 10);
+    $("#line-right").barfiller({});
+    $("#dps-right").barfiller({});
+    $("#push-right").barfiller({});
+    $("#nuke-right").barfiller({});
+    $("#durable-right").barfiller({});
+    $("#initial-right").barfiller({});
+    $("#disable-right").barfiller({});
+    $("#healing-right").barfiller({});
 }
 
 $(document).ready(function(){
@@ -313,106 +375,394 @@ function check_if_can_compare() {
     }
 }
 
+function drawCircleChart(id){
+    $(id).empty().append("<p class='percent-text'>" + $(id).data('percent') + "%</p>");
+    addOneBar(id);
+}
+  
+function addOneBar(id){
+    var percent = $(id).data('percent');
+    var noOfBars = .36*percent;
+    if( $(id).children().length-1 < noOfBars ){
+      $(id).append('<div class="bar"></div>');
+        setTimeout(function() { addOneBar(id); },0);
+    }
+}
+
 $(document).ready(function() {
-        var width = document.getElementById('left-team').offsetWidth / 8;
-        //alert(width);
-		var height = width;
-		var dataset = [36 , 64];
-		
-		var svg = d3.selectAll("#win-rate-left")
-                    //.append("svg")
-					.attr("width", width)
-					.attr("height", width);
-		
-		var pie = d3.layout.pie();
-
-		var piedata = pie(dataset);
-		
-		var outerRadius = width / 2;	//外半径
-		var innerRadius = width / 2 - (width < 30 ? 2 : 3);	//内半径，为0则中间没有空白
-
-		var arc = d3.svg.arc()	//弧生成器
-					.innerRadius(innerRadius)	//设置内半径
-					.outerRadius(outerRadius);	//设置外半径
-		
-		var color = d3.scale.category10();
-		
-		var arcs = svg.selectAll("g")
-					  .data(piedata)
-					  .enter()
-					  .append("g")
-					  .attr("transform","translate("+ (width/2) +","+ (width/2) +")");
-					  
-		arcs.append("path")
-			.attr("fill",function(d,i){
-				if (i < 1) {
-                    return "dimgray";
-                } else {
-                    return "green";
-                }
-			})
-			.attr("d",function(d){
-				return arc(d);
-			});
-		
-		arcs.append("text")
-            .attr("text-anchor","middle")
-            .attr("transform","translate("+ 0 +","+ (width/9) +")")
-            .attr("font-size", width / 4)
-			.text(function() {
-                return dataset[1] + "%";
-            });
+    var i=0;
+    $('.circle-chart').each(function(){
+                var id = 'circle-chart' + i;
+                $(this).attr('id',id);
+                drawCircleChart('#'+id);
+                i++;
+        })
+  
+        $('.circle-chart').click(function(){
+            var thisId = $(this).attr('id');
+            drawCircleChart('#'+thisId);
+        }) 
+        
     window.onresize = function() {
-        d3.selectAll("#win-rate-left").remove();
-        var width = document.getElementById('left-team').offsetWidth / 8;
-		var height = width;
-		var dataset = [36 , 64];
-		
-		var svg = d3.selectAll("#win-rate")
-                    .append("svg")
-                    .attr("id", "win-rate-left")
-					.attr("width", width)
-					.attr("height", width);
-		
-		var pie = d3.layout.pie();
+        
+        // attribute chart
+        d3.selectAll("#attribute-compare-detail").remove();
+        var margin = {top: 10, right: 10, bottom: 20, left: 10},
+    width = document.getElementById('attribute-compare-chart').offsetWidth - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
 
-		var piedata = pie(dataset);
-		
-		var outerRadius = width / 2;  //外半径
-		var innerRadius = width / 2 - (width < 30 ? 2 : 3);	//内半径，为0则中间没有空白
+    width = width,
+    height = height;
 
-		var arc = d3.svg.arc()	//弧生成器
-					.innerRadius(innerRadius)	//设置内半径
-					.outerRadius(outerRadius);	//设置外半径
-		
-		var color = d3.scale.category10();
-		
-		var arcs = svg.selectAll("g")
-					  .data(piedata)
-					  .enter()
-					  .append("g")
-					  .attr("transform","translate("+ (width/2) +","+ (width/2) +")");
-					  
-		arcs.append("path")
-			.attr("fill",function(d,i){
-				if (i < 1) {
-                    return "dimgray";
-                } else {
-                    return "green";
-                }
-			})
-			.attr("d",function(d){
-				return arc(d);
-			});
-		
-		arcs.append("text")
-            .attr("text-anchor","middle")
-            .attr("transform","translate("+ 0 +","+ (width/9) +")")
-            .attr("font-size", width / 4)
-			.text(function() {
-                return dataset[1] + "%";
+    headerColumn = "Income Bracket",
+    leftColumn = "Obama",
+    rightColumn = "Romney";
+
+    var x = d3.scale.linear()
+                .range([0, width])
+
+    var y = d3.scale.ordinal()
+                .rangeRoundBands([0, height], .2);
+
+        var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .ticks(30)
+                    // Positive values on both sides
+                    .tickFormat(function(d) { return (d<0) ? -1*d : d; })
+                    .orient("bottom");
+    var yAxis = d3.svg.axis()
+                        .scale(y)
+                        .orient("left")
+                        .tickSize(0)
+                        .tickPadding(6);
+
+    var svg = d3.select("#attribute-compare-chart")
+                        .append("svg")
+                        .attr("id", "attribute-compare-detail")
+                        .attr({
+                            "width": width + margin.left + margin.right,
+                            "height": height + margin.top + margin.bottom
+                        })
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("data.csv", function(error, data) {
+            data.forEach(function(d) {
+                d[leftColumn] = parseFloat(Math.abs(d[leftColumn])),
+                d[rightColumn] = parseFloat(Math.abs(d[rightColumn]));
             });
-        }
+
+                    x.domain([-15,15]).nice();
+                    y.domain(data.map(function(d) { return d[headerColumn]; }));
+
+                var bar = svg.selectAll(".bar").data(data).enter()
+
+                // Right bar
+                bar.append("rect")
+                    .attr({
+                        "class": "bar right",
+                        "x": x(0),
+                        "y": function(d) { return y(d[headerColumn]); },
+                        "width": function(d) {
+                                return Math.abs(x(d[rightColumn]) - x(0)); },
+                        "height": y.rangeBand()
+                });
+                // Left bar
+                bar.append("rect")
+                    .attr({
+                        "class": "bar left",
+                        "x": function(d) { return x(-d[leftColumn]); },
+                        "y": function(d) { return y(d[headerColumn]); },
+                        "width": function(d) {
+                                return Math.abs(x(d[leftColumn]) - x(0)); },
+                        "height": y.rangeBand()
+                });
+        svg.append("g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + x(1) + ",0)")
+                .call(yAxis);
+        svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+
+
+//                svg.append("g")
+//                    .attr("class", "x axis")
+//                    .call(xAxis);
+
+                    /** TODO: Labels/Tooltips */
+
+//                svg.append("g")
+//                    .attr({
+//                        "class": "grid",
+//                        "transform": "translate(0," + height + ")"
+//                    })
+//                    .call(xAxis
+//                    .tickSize(height, 0, 0)
+//                    .tickFormat("")
+//                );
+            });
+
+    }
 })
+
+$(document).ready(function() {
+    var margin = {top: 10, right: 10, bottom: 20, left: 10},
+    width = document.getElementById('attribute-compare-chart').offsetWidth - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
+
+    width = width,
+    height = height;
+
+    headerColumn = "Income Bracket",
+    leftColumn = "Obama",
+    rightColumn = "Romney";
+
+    var x = d3.scale.linear()
+                .range([0, width])
+
+    var y = d3.scale.ordinal()
+                .rangeRoundBands([0, height], .2);
+
+    var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .ticks(30)
+                    // Positive values on both sides
+                    .tickFormat(function(d) { return (d<0) ? -1*d : d; })
+                    .orient("bottom");
+    var yAxis = d3.svg.axis()
+                        .scale(y)
+                        .orient("left")
+                        .tickSize(0)
+                        .tickPadding(6);
+
+    var svg = d3.select("#attribute-compare-chart")
+                        .append("svg")
+                        .attr("id", "attribute-compare-detail")
+                        .attr({
+                            "width": width + margin.left + margin.right,
+                            "height": height + margin.top + margin.bottom
+                        })
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("data.csv", function(error, data) {
+            data.forEach(function(d) {
+                d[leftColumn] = parseFloat(Math.abs(d[leftColumn])),
+                d[rightColumn] = parseFloat(Math.abs(d[rightColumn]));
+            });
+
+                    x.domain([-15,15]).nice();
+                    y.domain(data.map(function(d) { return d[headerColumn]; }));
+
+                var bar = svg.selectAll(".bar").data(data).enter()
+
+                // Right bar
+                bar.append("rect")
+                    .attr({
+                        "class": "bar right",
+                        "x": x(0),
+                        "y": function(d) { return y(d[headerColumn]); },
+                        "width": function(d) {
+                                return Math.abs(x(d[rightColumn]) - x(0)); },
+                        "height": y.rangeBand()
+                });
+                // Left bar
+                bar.append("rect")
+                    .attr({
+                        "class": "bar left",
+                        "x": function(d) { return x(-d[leftColumn]); },
+                        "y": function(d) { return y(d[headerColumn]); },
+                        "width": function(d) {
+                                return Math.abs(x(d[leftColumn]) - x(0)); },
+                        "height": y.rangeBand()
+                });
+        svg.append("g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + x(1) + ",0)")
+                .call(yAxis);
+
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+            });
+    
+    
+    var test = ['test', 2.4];
+    var chart = c3.generate({
+            bindto: '#chart3',
+            data: {
+                columns: [
+                            test
+                        ],
+                type: 'gauge',
+            },
+            gauge: {
+                max: 10,
+            },
+            color: {
+                pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+                threshold: {
+                    //            unit: 'value', // percentage is default
+                    //            max: 200, // 100 is default
+                    values: [2, 5, 8, 10]
+                }
+            },
+            size: {
+                height: 60
+            }
+        
+    });
+    
+    var chart1 = c3.generate({
+        bindto: '#chart1',
+        data: {
+            columns: [
+                ['win-rate', 21.4]
+            ],
+            type: 'gauge',
+        },
+        gauge: {
+            max: 10, // 100 is default
+        },
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            threshold: {
+//            unit: 'value', // percentage is default
+//            max: 200, // 100 is default
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 60
+        }
+        
+    });
+    var chart2 = c3.generate({
+        bindto: '#chart2',
+        data: {
+            columns: [
+                ['win-rate', 61.4]
+            ],
+            type: 'gauge',
+        },
+        gauge: {},
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three     color levels for the percentage values.
+            threshold: {
+//            unit: 'value', // percentage is default
+//            max: 200, // 100 is default
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 60
+        }
+        
+    });
+    var chart4 = c3.generate({
+        bindto: '#chart4',
+        data: {
+            columns: [
+                ['win-rate', 61.4]
+            ],
+            type: 'gauge',
+        },
+        gauge: {},
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            threshold: {
+                unit: 'value', // percentage is default
+                max: 200, // 100 is default
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 60
+        }   
+    });
+    var chart8 = c3.generate({
+        bindto: '#chart8',
+        data: {
+            columns: [
+                ['win-rate', 61.4]
+            ],
+            type: 'gauge',
+        },
+        gauge: {},
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            threshold: {
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 60
+        }   
+    });
+    var chart5 = c3.generate({
+        bindto: '#chart5',
+        data: {
+            columns: [
+                ['win-rate', 61.4]
+            ],
+            type: 'gauge',
+        },
+        gauge: {},
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            threshold: {
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 60
+        }   
+    });
+    var chart6 = c3.generate({
+        bindto: '#chart6',
+        data: {
+            columns: [
+                ['win-rate', 61.4]
+            ],
+            type: 'gauge',
+        },
+        gauge: {},
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            threshold: {
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 60
+        }   
+    });
+    var chart7 = c3.generate({
+        bindto: '#chart7',
+        data: {
+            columns: [
+                ['win-rate', 61.4]
+            ],
+            type: 'gauge',
+        },
+        gauge: {},
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            threshold: {
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 60
+        }   
+    });
+})
+
+
+
+
 
 
