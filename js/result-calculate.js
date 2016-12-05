48c1563594c6897a1bcee153ea4c8db4f1214f37
BelieverW <hlwang0309@hotmail.com>
2016-12-04 19:38:07 -0500

    last version I hope

--- js/result-calculate.js
+++ js/result-calculate.js
@@ -102,7 +102,7 @@ function calculateIndex(leftHero, rightHero) {
         laneJungle += laneCombo[0][1] + laneCombo[1][1] + laneCombo[2][2] * 0.7;
         lane.push(laneMid + Math.max(laneJungle, laneNoJungle));
     }
-    var theta = 2.0 / (1 + Math.exp((lane[1] - lane[0]) / 17.5));
+    var theta = 2.0 / (1 + Math.exp((lane[1] - lane[0]) / 3940.5));
 
     var left = radiant.slice();
     var right = dire.slice();
@@ -115,6 +115,12 @@ function calculateIndex(leftHero, rightHero) {
         for (var j = 0; j < 7; j++) {
             var tmp = [];
             var attr = 0;
+            if (i == 0){
+                var thetaTeam = theta;
+            }
+            else {
+                var thetaTeam = 2 - theta;
+            }
             if (columns[j] == 'initial' || columns[j] == 'healing') {
                 var para = [0.75, 0.25];
             }
@@ -134,7 +140,7 @@ function calculateIndex(leftHero, rightHero) {
             for (var p = 0; p < para.length; p++) {
                 attr += tmp[p] * para[p];
             }
-            team.push(theta*attr/math.mean(para)/para.length);
+            team.push(thetaTeam*attr/math.mean(para)/para.length);
         }
         var srAlly = [];
         var srEnemy = [];
@@ -162,8 +168,30 @@ function calculateIndex(leftHero, rightHero) {
     return [teams, sr];
 }
 
-function winRateCal(teams) {
-    var weight = [0.5, 0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 6, 25.817];
+function winRateCalNormal(teams) {
+    var weight = [3.04639292022, 0.1, 0.1, 0.1, 0.113614242375, 0.1, 0.617461336455, 3.82253150095, 12.6568574805];
+    var sigma = Math.sqrt(Math.pow(teams[0][8], 2) + Math.pow(teams[1][8], 2))*weight[8];
+    var mu = 0;
+    for (var i = 0; i < weight.length-1; i++){
+        mu += weight[i]*(teams[0][i] - teams[1][i])
+    }
+    var winrate = 1-compute(0, mu, sigma);
+    return [winrate, 1 - winrate];
+}
+
+function winRateCalHigh(teams) {
+    var weight = [2.04639292022, 0.20612743936, 0.2329069516, 1.1510112076, 0.113614242375, 0.1, 0.12531500951, 3.82253150095, 14.3565374712];
+    var sigma = Math.sqrt(Math.pow(teams[0][8], 2) + Math.pow(teams[1][8], 2))*weight[8];
+    var mu = 0;
+    for (var i = 0; i < weight.length-1; i++){
+        mu += weight[i]*(teams[0][i] - teams[1][i])
+    }
+    var winrate = 1-compute(0, mu, sigma);
+    return [winrate, 1 - winrate];
+}
+
+function winRateCalVeryHigh(teams) {
+    var weight = [1.94510112076,0.1,0.1,0.98612743936,0.1,0.1,0.699445303566,3.96932613631,13.489694369];
     var sigma = Math.sqrt(Math.pow(teams[0][8], 2) + Math.pow(teams[1][8], 2))*weight[8];
     var mu = 0;
     for (var i = 0; i < weight.length-1; i++){
@@ -194,59 +222,43 @@ function compare() {
         leftAnti = calculateResult[1][1];
         rightCombo = calculateResult[1][2];
         rightAnti = calculateResult[1][3];
-        indexResult = [{
-            attribute: "DPS",
-            left: teams[0][0],
-            right: teams[1][0]
-        },
-            {
-                attribute: "PUSH",
-                left: teams[0][1],
-                right: teams[1][1]
-            },
-            {
-                attribute: "NUKE",
-                left: teams[0][2],
-                right: teams[1][2]
-            },
-            {
-                attribute: "DURABLE",
-                left: teams[0][3],
-                right: teams[1][3]
-            },
-            {
-                attribute: "DISABLE",
-                left: teams[0][4],
-                right: teams[1][4]
-            },
-            {
-                attribute: "INITIAL",
-                left: teams[0][5],
-                right: teams[1][5]
-            },
-            {
-                attribute: "HEALING",
-                left: teams[0][6],
-                right: teams[1][6]
-            },
-            {
-                attribute: "ADVANCE-INDEXING",
-                left: teams[0][7],
-                right: teams[1][7]
-            }];
+
         //alert(indexResult);
         var result = indexResult;
-        var win = winRateCal(teams);
+        var winNormal = winRateCalNormal(teams);
         // alert(win[0].toFixed(2)*100);
-        var winLeft = parseFloat(win[0] * 100).toFixed(1);
-        var winRight = parseFloat(win[1]* 100).toFixed(1)
+        var winLeftNormal = parseFloat(winNormal[0] * 100).toFixed(1);
+        var winRightNormal = parseFloat(winNormal[1]* 100).toFixed(1)
 
-        $(".win-rate-left").empty().append("<p class='percent-text'>" + winLeft + "%</p>");
-        addOneBar(win[0].toFixed(2)*100, ".win-rate-left");
-        $(".win-rate-right").empty().append("<p class='percent-text'>" + winRight + "%</p>");
-        addOneBar(win[0].toFixed(2)*100, ".win-rate-right");
+        $(".normal-left").empty().append("<p class='percent-text'>" + winLeftNormal + "%</p>");
+        addOneBar(winNormal[0].toFixed(2)*100, ".normal-left");
+        $(".normal-right").empty().append("<p class='percent-text'>" + winRightNormal + "%</p>");
+        addOneBar(winNormal[1].toFixed(2)*100, ".normal-right");
+        
+        
+        var winHigh = winRateCalHigh(teams);
+        // alert(win[0].toFixed(2)*100);
+        var winLeftHigh = parseFloat(winHigh[0] * 100).toFixed(1);
+        var winRightHigh = parseFloat(winHigh[1]* 100).toFixed(1)
 
-        drawCompareChart2(result);
+        $(".high-left").empty().append("<p class='percent-text'>" + winLeftHigh + "%</p>");
+        addOneBar(winHigh[0].toFixed(2)*100, ".high-left");
+        $(".high-right").empty().append("<p class='percent-text'>" + winRightHigh + "%</p>");
+        addOneBar(winHigh[1].toFixed(2)*100, ".high-right");
+        
+        
+        var winVeryHigh = winRateCalVeryHigh(teams);
+        // alert(win[0].toFixed(2)*100);
+        var winLeftVeryHigh = parseFloat(winVeryHigh[0] * 100).toFixed(1);
+        var winRightVeryHigh = parseFloat(winVeryHigh[1]* 100).toFixed(1)
+
+        $(".veryHigh-left").empty().append("<p class='percent-text'>" + winLeftVeryHigh + "%</p>");
+        addOneBar(winVeryHigh[0].toFixed(2)*100, ".veryHigh-left");
+        $(".veryHigh-right").empty().append("<p class='percent-text'>" + winRightVeryHigh + "%</p>");
+        addOneBar(winVeryHigh[1].toFixed(2)*100, ".veryHigh-right");
+        
+
+        drawCompareChart(teams);
     }
 }
 
@@ -258,7 +270,7 @@ function addOneBar(num, className) {
     }
 }
 
-function drawCompareChart(data) {
+function drawCompareChart2(data) {
     d3.selectAll("#attribute-compare-detail").remove();
     var margin = {top: 10, right: 10, bottom: 20, left: 10},
         width = document.getElementById('attribute-compare-chart').offsetWidth - margin.left - margin.right,
@@ -345,91 +357,115 @@ function drawCompareChart(data) {
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide);
 }
-function drawCompareChart2(data) {
-    d3.selectAll("#attribute-compare-detail").remove();
-    var margin = {top: 10, right: 10, bottom: 20, left: 10},
-        width = $(window).width() - margin.left - margin.right,
-        height = 200 - margin.top - margin.bottom;
-    //alert(width);
+function drawCompareChart(data) {
 
-    width = width,
-        height = height;
+    $("#dps-left-val").text(data[0][0].toFixed(2));
+    $("#push-left-val")
+        .text(data[0][1].toFixed(2));
+    $("#nuke-left-val")
+        .text(data[0][2].toFixed(2));
+    $("#durable-left-val")
+        .text(data[0][3].toFixed(2));
+    $("#disable-left-val")
+        .text(data[0][4].toFixed(2));
+    $("#initial-left-val")
+        .text(data[0][5].toFixed(2));
+    $("#healing-left-val")
+        .text(data[0][6].toFixed(2));
+    $("#ac-left-val")
+        .text(data[0][7].toFixed(2));
+    $("#dps-right-val")
+        .text(data[1][0].toFixed(2));
+    $("#push-right-val")
+        .text(data[1][1].toFixed(2));
+    $("#nuke-right-val")
+        .text(data[1][2].toFixed(2));
+    $("#durable-right-val")
+        .text(data[1][3].toFixed(2));
+    $("#disable-right-val")
+        .text(data[1][4].toFixed(2));
+    $("#initial-right-val")
+        .text(data[1][5].toFixed(2));
+    $("#healing-right-val")
+        .text(data[1][6].toFixed(2));
+    $("#ac-right-val")
+        .text(data[1][7].toFixed(2));
+    //alert(data[0][7]);
 
-    headerColumn = "attribute",
-        leftColumn = "left",
-        rightColumn = "right";
+    var columns = ['dps', 'push', 'nuke', 'durable', 'disable', 'initial', 'healing', 'ac', 'var'];
 
-    var x = d3.scale.linear()
-        .range([0, width])
+    d3.select("#dps-left-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[0][0] / 15 * 100).toFixed(2));
+    d3.select("#push-left-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[0][1] / 15 * 100).toFixed(2));
+    d3.select("#nuke-left-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[0][2] / 15 * 100).toFixed(2));
+    d3.select("#durable-left-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[0][3] / 15 * 100).toFixed(2));
+    d3.select("#disable-left-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[0][4] / 15 * 100).toFixed(2));
+    d3.select("#initial-left-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[0][5] / 15 * 100).toFixed(2));
+    d3.select("#healing-left-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[0][6] / 15 * 100).toFixed(2));
+    d3.select("#ac-left-bar")
+        .select(".fill")
+        .attr("data-percentage", ((data[0][7] + 4) / 8 * 100).toFixed(2));
+    //$("#dps-left-bar").barfiller2({barColor: function() {if (data[0][0] > data[1][0]) {return "#FFFF00"} else {return "C0C0C0"}}});
+    //$("#push-left-bar").barfiller2({barColor: function() {if (data[0][1] > data[1][1]) {return "#FFFF00"} else {return "C0C0C0"}}});
+    //$("#nuke-left-bar").barfiller2({barColor: function() {if (data[0][2] > data[1][2]) {return "#FFFF00"} else {return "C0C0C0"}}});
+    //$("#durable-left-bar").barfiller2({barColor: function() {if (data[0][3] > data[1][3]) {return "#FFFF00"} else {return "C0C0C0"}}});
+    //$("#initial-left-bar").barfiller2({barColor: function() {if (data[0][5] > data[1][4]) {return "#FFFF00"} else {return "C0C0C0"}}});
+    //$("#disable-left-bar").barfiller2({barColor: function() {if (data[0][4] > data[1][5]) {return "#FFFF00"} else {return "C0C0C0"}}});
+    //$("#healing-left-bar").barfiller2({barColor: function() {if (data[0][6] > data[1][6]) {return "#FFFF00"} else {return "C0C0C0"}}});
+    //$("#ac-left-bar").barfiller2({barColor: function() {if (data[0][7] > data[1][7]) {return "#FFFF00"} else {return "C0C0C0"}}});
+    $("#dps-left-bar").barfiller2({barColor: '#FF8000'});
+    $("#push-left-bar").barfiller2({barColor: '#FF8000'});
+    $("#nuke-left-bar").barfiller2({barColor: '#FF8000'});
+    $("#durable-left-bar").barfiller2({barColor: '#FF8000'});
+    $("#initial-left-bar").barfiller2({barColor: '#FF8000'});
+    $("#disable-left-bar").barfiller2({barColor: '#FF8000'});
+    $("#healing-left-bar").barfiller2({barColor: '#FF8000'});
+    $("#ac-left-bar").barfiller2({barColor: '#FF8000'});
 
-    var y = d3.scale.ordinal()
-        .rangeRoundBands([0, height], .2);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .ticks(30)
-        // Positive values on both sides
-        .tickFormat(function(d) { return (d<0) ? -1*d : d; })
-        .orient("bottom");
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .tickSize(0)
-        .tickPadding(6);
-
-    var svg = d3.select("#attribute-compare-chart")
-        .append("svg:svg")
-        .attr("id", "attribute-compare-detail")
-        .attr({
-            "width": width + margin.left + margin.right,
-            "height": height + margin.top + margin.bottom
-        })
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    data.forEach(function(d) {
-        d[leftColumn] = parseFloat(Math.abs(d[leftColumn])),
-            d[rightColumn] = parseFloat(Math.abs(d[rightColumn]));
-    });
-
-    x.domain([-15,15]).nice();
-    y.domain(data.map(function(d) { return d[headerColumn]; }));
-
-    var bar = svg.selectAll(".bar").data(data).enter()
-
-    /* Initialize tooltip */
-    tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
-        return  d[headerColumn] + ":<br><span>left " +
-            "<span style='color:red'>" +
-            d[leftColumn].toFixed(2) + "</span> right " +
-            "<span style='color:red'>" +
-            d[rightColumn].toFixed(2) + " </span></span>"; });
-
-    /* Invoke the tip in the context of your visualization */
-    svg.call(tip)
-
-    // Right bar
-    bar.append("rect")
-        .attr({
-            "class": "bar right",
-            "x": x(0),
-            "y": function(d) { return y(d[headerColumn]); },
-            "width": function(d) {
-                return Math.abs(x(d[rightColumn]) - x(0)); },
-            "height": y.rangeBand()
-        })
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
-    // Left bar
-    bar.append("rect")
-        .attr({
-            "class": "bar left",
-            "x": function(d) { return x(-d[leftColumn]); },
-            "y": function(d) { return y(d[headerColumn]); },
-            "width": function(d) {
-                return Math.abs(x(d[leftColumn]) - x(0)); },
-            "height": y.rangeBand()
-        })
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
+    d3.select("#dps-right-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[1][0] / 15 * 100).toFixed(2));
+    d3.select("#push-right-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[1][1] / 15 * 100).toFixed(2));
+    d3.select("#nuke-right-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[1][2] / 15 * 100).toFixed(2));
+    d3.select("#durable-right-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[1][3] / 15 * 100).toFixed(2));
+    d3.select("#disable-right-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[1][4] / 15 * 100).toFixed(2));
+    d3.select("#initial-right-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[1][5] / 15 * 100).toFixed(2));
+    d3.select("#healing-right-bar")
+        .select(".fill")
+        .attr("data-percentage", (data[1][6] / 15 * 100).toFixed(2));
+    d3.select("#ac-right-bar")
+        .select(".fill")
+        .attr("data-percentage", ((data[1][7] + 5) / 10 * 100).toFixed(2));
+    $("#dps-right-bar").barfiller2({barColor: function() {if (data[0][0] <= data[1][0]) {return "#FFFF00"} else {return "#FFFF00"}}});
+    $("#push-right-bar").barfiller2({barColor: function() {if (data[0][1] <= data[1][1]) {return "#FFFF00"} else {return "#FFFF00"}}});
+    $("#nuke-right-bar").barfiller2({barColor: function() {if (data[0][2] <= data[1][2]) {return "#FFFF00"} else {return "#FFFF00"}}});
+    $("#durable-right-bar").barfiller2({barColor: function() {if (data[0][3] <= data[1][3]) {return "#FFFF00"} else {return "#FFFF00"}}});
+    $("#initial-right-bar").barfiller2({barColor: function() {if (data[0][5] <= data[1][5]) {return "#FFFF00"} else {return "#FFFF00"}}});
+    $("#disable-right-bar").barfiller2({barColor: function() {if (data[0][4] <= data[1][4]) {return "#FFFF00"} else {return "#FFFF00"}}});
+    $("#healing-right-bar").barfiller2({barColor: function() {if (data[0][6] <= data[1][6]) {return "#FFFF00"} else {return "#FFFF00"}}});
+    $("#ac-right-bar").barfiller2({barColor: function() {if (data[0][7] <= data[1][7]) {return "#FFFF00"} else {return "#FFFF00"}}});
 }
     
